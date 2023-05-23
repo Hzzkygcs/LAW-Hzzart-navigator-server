8a1fe86e526b16e0347c06e6a6c0c4d51c2cdfef
Immanuel01 <immanuel01@ui.ac.id>
2023-05-23 14:31:29 +0700

    feat: Implement visit consul

--- routes/visit.js
+++ routes/visit.js
@@ -4,12 +4,15 @@ const {json} = require("express");
 const route = express.Router();
 
 route.get("/visit/:env/:port", async (req, res) => {
-    const key = req.path.env;
+    const envKey = req.params.env;
+    const port = req.params.port;
+    console.log(`visited ${envKey}:${port}`)
+    const key = envKey;
     if (!process.env[key]) {
         res.status(404);
         return res.send("Not found")
     }
-    res.redirect(`http://${process.env[key]}:${req.path.port}`);
+    res.redirect(`http://${process.env[key]}:${port}`);
 });
 
 
